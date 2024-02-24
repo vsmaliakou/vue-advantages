596515cce0fb1287d99a25ba327528fcf559a131
vsmaliakou <vsmaliakou@yandex.by>
2024-02-24 09:40:53 +0300

    FEATURE: Vuex Basics in Vue 3 with Composition API

---
+++ vuex-basics/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <OCounter />
+  <CCounter />
+</template>
+
+<script>
+import OCounter from "@/components/OCounter.vue";
+import CCounter from "@/components/CCounter.vue";
+
+export default {
+  components: { OCounter, CCounter },
+};
+</script>
